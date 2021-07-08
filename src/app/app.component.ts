c1e0cdba9d8bc84360ea3c2afae6bea0b8c6cb7d
Kushan <kushanthsliit@gmail.com>
2021-07-08 22:46:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Maps, Zoom, Marker, NavigationLine } from '@syncfusion/ej2-angular-maps';
+
+Maps.Inject(Zoom, Marker, NavigationLine);
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  public layerType: string = 'OSM';
+
+  public zoomSettings: object = {
+      zoomFactor: 7,
+      enable : true
+  };
+  
+  public centerPosition: object = {
+      latitude: 8.233,
+       longitude: 80.354,
+  };
+  
+   public markerSettings: object = [{
+       visible: true,
+       height: 35,
+       width: 25,
+       dataSource: [
+           {
+               latitude: 7.21984,
+               longitude: 79.84537,
+               name: "Negombo"
+           },
+           {
+            latitude: 8.34427,
+            longitude: 80.41080,
+            name: "Anurathapura"
+           },
+           {
+               latitude: 9.08550,
+               longitude: 79.82343,
+               name: "Mannar"
+           },
+       ]
+   }];
+   public navigationLineSettings: object = [{
+       visible: true,
+       color: "blue",
+       width: 5,
+       angle: 0.1,
+       latitude: [7.21984, 8.34427, 9.08550],
+       longitude: [ 79.84537, 80.41080, 79.82343]
+   }];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MapsModule } from '@syncfusion/ej2-angular-maps';
+import { LegendService, MarkerService, MapsTooltipService, DataLabelService, BubbleService, NavigationLineService, SelectionService, AnnotationsService, ZoomService } from '@syncfusion/ej2-angular-maps';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MapsModule
   ],
-  providers: [],
+  providers: [LegendService, MarkerService, MapsTooltipService, DataLabelService, BubbleService, NavigationLineService , SelectionService, AnnotationsService, ZoomService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
